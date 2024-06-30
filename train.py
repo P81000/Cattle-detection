0a9b2d908d8301f3e1d98a339831d05f298a8434
P81000 <p.masteguin@unifesp.br>
2024-06-30 18:24:46 -0300

    trying

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import yolov5
 import os
 import glob
 import torch
@@ -9,14 +10,16 @@ import numpy as np
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 from tqdm import tqdm
-import yolov5
+
 
 class CowDataset(Dataset):
     def __init__(self, root_dir, transform=None):
         self.root_dir = root_dir
         self.transform = transform
-        self.image_paths = sorted(glob.glob(os.path.join(root_dir, '*.jpg')))
-        self.label_paths = sorted(glob.glob(os.path.join(root_dir, '*.txt')))
+        self.image_paths = glob.glob(os.path.join(root_dir, '*.jpg'))
+        self.label_paths = glob.glob(os.path.join(root_dir, '*.txt'))
+        self.image_paths.sort()
+        self.label_paths.sort()
 
     def __len__(self):
         return len(self.image_paths)
@@ -28,24 +31,22 @@ class CowDataset(Dataset):
         image = cv2.imread(img_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        height, width = image.shape[:2]
-
         boxes = []
         with open(label_path, 'r') as f:
             lines = f.readlines()
             for line in lines:
                 parts = line.strip().split()
-                cls, x_center, y_center, box_width, box_height = map(float, parts)
-                x_center /= width
-                y_center /= height
-                box_width /= width
-                box_height /= height
-                boxes.append([x_center, y_center, box_width, box_height, int(cls)])
+                cls, x_center, y_center, width, height = map(float, parts)
+                x_min = (x_center - width / 2) * image.shape[1]
+                y_min = (y_center - height / 2) * image.shape[0]
+                x_max = (x_center + width / 2) * image.shape[1]
+                y_max = (y_center + height / 2) * image.shape[0]
+                boxes.append([x_min, y_min, x_max, y_max, int(cls)])
 
         boxes = np.array(boxes)
 
         if self.transform:
-            augmented = self.transform(image=image, bboxes=boxes[:, :4], class_labels=boxes[:, 4])
+            augmented = self.transform(image=image, bboxes=boxes, class_labels=boxes[:, 4])
             image = augmented['image']
             boxes = np.concatenate([augmented['bboxes'], np.expand_dims(boxes[:, 4], axis=1)], axis=1)
 
@@ -55,15 +56,16 @@ class CowDataset(Dataset):
 
         return image, target
 
+
 transform = A.Compose(
     [
-        A.Resize(300, 300),
+        A.Resize(640, 640),
         A.HorizontalFlip(p=0.5),
         A.VerticalFlip(p=0.5),
         A.RandomBrightnessContrast(p=0.2),
         ToTensorV2()
     ],
-    bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels'])
+    bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels'])
 )
 
 train_dir = './dataset/images/train'
@@ -74,14 +76,10 @@ train_dataset = CowDataset(root_dir=train_dir, transform=transform)
 val_dataset = CowDataset(root_dir=val_dir, transform=transform)
 test_dataset = CowDataset(root_dir=test_dir, transform=transform)
 
-def collate_fn(batch):
-    images, targets = zip(*batch)
-    images = torch.stack(images, dim=0)
-    return images, targets
+train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, collate_fn=lambda x: tuple(zip(*x)))
+val_loader = DataLoader(val_dataset, batch_size=8, shuffle=False, collate_fn=lambda x: tuple(zip(*x)))
+test_loader = DataLoader(test_dataset, batch_size=8, shuffle=False, collate_fn=lambda x: tuple(zip(*x)))
 
-train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, collate_fn=collate_fn)
-val_loader = DataLoader(val_dataset, batch_size=8, shuffle=False, collate_fn=collate_fn)
-test_loader = DataLoader(test_dataset, batch_size=8, shuffle=False, collate_fn=collate_fn)
 
 model = yolov5.load('yolov5s.pt')
 
@@ -95,7 +93,7 @@ for epoch in range(epochs):
     model.train()
 
     for images, targets in tqdm(train_loader):
-        images = images.to('cuda')
+        images = torch.stack(images).to('cuda')
         targets = [{k: v.to('cuda') for k, v in t.items()} for t in targets]
 
         loss_dict = model(images, targets)
@@ -108,7 +106,7 @@ for epoch in range(epochs):
     model.eval()
     with torch.no_grad():
         for images, targets in tqdm(val_loader):
-            images = images.to('cuda')
+            images = torch.stack(images).to('cuda')
             targets = [{k: v.to('cuda') for k, v in t.items()} for t in targets]
 
             loss_dict = model(images, targets)
@@ -120,7 +118,7 @@ for epoch in range(epochs):
 model.eval()
 with torch.no_grad():
     for images, targets in tqdm(test_loader):
-        images = images.to('cuda')
+        images = torch.stack(images).to('cuda')
         targets = [{k: v.to('cuda') for k, v in t.items()} for t in targets]
 
         results = model(images, targets)
@@ -128,4 +126,3 @@ with torch.no_grad():
 print(results)
 
 model.save('./result/yolov5s_cow_final.pt')
-
