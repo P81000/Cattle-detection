aba2238d0a63522ef45815e23671e158b77fc705
Ubuntu <ubuntu@ip-172-31-3-47.sa-east-1.compute.internal>
2024-07-01 19:44:52 +0000

    Final test

---
+++ test.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+
+model = YOLO("/home/ubuntu/Cattle-detection/runs/detect/train2/weights/best.pt")
+
+source = "finalcounttest.mp4"
+
+results = model(source, save=True, imgsz=320, conf=0.5)
